eeada65ba6c6f8fa264c59d0e182d9fb1774d43b
Syafiraz <2310631170052@student.unsika.ac.id>
2024-11-26 22:39:31 +0700

    Add files via upload

---
+++ Tugas2/MainLaguBingo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package TugasPraktikum2Bingo;
+
+import TugasPraktikum2Bingo.*;
+
+/**
+ *
+ * @author Auriga
+ */
+public class MainLaguBingo {
+    public static void main(String[] args) {
+        new LaguBingo1(); 
+    }
+}
